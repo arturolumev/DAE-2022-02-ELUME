1e7a32fffdaeb2bc37323e15908725b91686958f
Arturo Lume <edwin.lume@tecsup.edu.pe>
2022-11-27 23:26:13 -0500

    React Lab 12

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PosPos from './modulos/pos/pages/PosPos';
+
+const App = () => {
+  return (
+    <PosPos/>
+    //<h1>Sistema de punto de venta</h1>
+  );
+}
+
+export default App
